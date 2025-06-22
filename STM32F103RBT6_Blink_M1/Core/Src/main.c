3144cac6ffccb4459dd2e47ca5582f5213e6ae42
Saranga Senarathna <saranganirwana@gmail.com>
2025-06-22 17:35:00 +0530

    V0.1(1)

--- STM32F103RBT6_Blink_M1/Core/Src/main.c
+++ STM32F103RBT6_Blink_M1/Core/Src/main.c
@@ -95,7 +95,8 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	  HAL_GPIO_TogglePin(LED0_GPIO_Port, LED0_Pin);
+	  HAL_Delay(500);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
